e93d7f6a1d8107097457ed4944337c822377ab9a
Karlla Souza <karlladanielly94@hotmail.com>
2015-11-14 16:42:41 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,14 @@
+class Animal {
+	static String nome;
+	static int idade;
+	static boolean corre;
+	static boolean voa;
+	
+	Animal(String nome, int idade, boolean corre, boolean voa){
+		Animal.nome=nome;
+		Animal.idade=idade;
+		Animal.corre=corre;
+		Animal.voa=voa;
+	}
+	
+}
