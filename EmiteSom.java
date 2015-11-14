eb089add423cc69d2b09ad883414c092504b6a96
Karlla Souza <karlladanielly94@hotmail.com>
2015-11-14 16:45:56 -0300

    Create EmiteSom.java

---
+++ EmiteSom.java
@@ -0,0 +1,11 @@
+class EmiteSom {
+	static void somDoAnimal_cachorro(){
+		System.out.print("    - Au...au...au!!!");
+	}
+	static void somDoAnimal_cavalo(){
+		System.out.print("    - Pruuuh!!Irriiririi!!!");
+	}
+	static void somDoAnimal_passaro(){
+		System.out.print("    - Fiu...fiu...fiu..");
+	}
+}
